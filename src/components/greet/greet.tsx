87de2e57aca30d03315aedbe8cf31a36b8d56a1a
Sachin Singh <sachinrk0412@gmail.com>
2023-09-11 01:01:24 +0530

    greet test component

---
+++ src/components/greet/greet.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type GreetProps = {
+    name?: string;
+}
+
+const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      Hello {props.name}
+    </div>
+  )
+}
+
+export default Greet;
